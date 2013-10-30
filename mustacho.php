ed9006d7d14bf1854978704af8c0ff281a83ed61
kziete <kziete@alVereUbuntu.(none)>
2013-10-30 17:42:42 -0300

    Subiendo un par de cambios

---
+++ mustacho.php
@@ -0,0 +1,18 @@
+<?php 
+class Mustacho{
+	public $templateDir = '/var/www/mongo/widgets/';
+	public $mustache;
+
+	public function __construct(){
+		require('mustache/src/Mustache/Autoloader.php');
+		Mustache_Autoloader::register();
+		$this->mustache = new Mustache_Engine;		
+	}
+
+	public function render($template, $hash){
+		return $this->mustache->render(
+			file_get_contents($this->templateDir . $template), 
+			$hash
+		);
+	}
+}
